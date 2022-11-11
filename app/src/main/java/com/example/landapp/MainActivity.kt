9a0032493c4f11152e37d0449335503e4cba163e
Kim <xzsa18@naver.com>
2022-11-11 16:32:16 +0900

    방 목록 추가 / 어댑터 연결

--- app/src/main/java/com/example/landapp/MainActivity.kt
+++ app/src/main/java/com/example/landapp/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.landapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.example.landapp.adapters.RoomAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val mRoomList = ArrayList<Room>()
+
+    lateinit var mRoomAdapter: RoomAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mRoomList.add(Room(8000, "서울시 동대문구", 3, "서울시 동대문구 3층방입니다"))
+        mRoomList.add(Room(24500, "원주시 단구동", 5, "원주시 단구동 5층방입니다"))
+        mRoomList.add(Room(16500, "용인시 김량장동", 7, "용인시 김량장동 7층방입니다"))
+        mRoomList.add(Room(10000, "원주시 명륜동", 4, "원주시 명륜동 4층방입니다"))
+        mRoomList.add(Room(7000, "원주시 치악로", 2, "원주시 치악로 2층방입니다"))
+
+        mRoomAdapter = RoomAdapter(this, R.layout.room_list_item,mRoomList)
+
+        roomListView.adapter = mRoomAdapter
     }
 }
