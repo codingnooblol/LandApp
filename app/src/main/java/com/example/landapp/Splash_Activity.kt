8afcd9dedd771868810b34f0821dbec52a4b0955
Kim <xzsa18@naver.com>
2022-11-12 16:20:55 +0900

    앱의 시작화면을 로딩화면(SplashActivity)로 변경함

---
+++ app/src/main/java/com/example/landapp/Splash_Activity.kt
@@ -0,0 +1,11 @@
+package com.example.landapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Splash_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
